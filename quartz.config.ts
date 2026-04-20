c01894579bf032c13f88ec076af4d3ad4b7ccdcf
matteo-qf <matteo@example.com>
2026-04-20 22:19:26 +0800

    rename: 曲阜知识库 -> Matteo Wiki

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "曲阜知识库",
+    pageTitle: "Matteo Wiki",
     pageTitleSuffix: " | Quartz",
     enableSPA: true,
     enablePopovers: true,
